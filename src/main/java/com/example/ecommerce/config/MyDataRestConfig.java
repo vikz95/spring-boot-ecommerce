f6a2b7bf4ad6e074eb88878b0555f984cf8570d5
vikz95 <viktor.zahariev@protonmail.com>
2020-06-16 12:13:59 +0200

    Tried to make the repositories read only, without success.
    Added sql scripts to reproduce the DB on your local machine

---
+++ src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.example.ecommerce.config;
+
+import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+// NOT WORKING!!!
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        // Temporarily disable these actions so that the entities will be READ only from REST api
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
