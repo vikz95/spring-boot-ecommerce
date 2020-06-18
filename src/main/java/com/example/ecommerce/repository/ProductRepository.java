0e14574de8322b43295717234691c06aa8ea7247
vikz95 <viktor.zahariev@protonmail.com>
2020-06-18 16:29:54 +0200

    Added Cross Origin support for the Angular frontend.

--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // Accept calls from browser scripts for this origin (protocol + hostname + port)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
