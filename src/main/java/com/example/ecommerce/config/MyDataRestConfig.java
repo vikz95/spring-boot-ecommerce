bd565d886916ba5510e1c7840ec60d86b7d34ddc
vikz95 <viktor.zahariev@protonmail.com>
2020-06-19 11:53:41 +0200

    Implemented search by category id AND exposed entities ids.

--- src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -2,19 +2,32 @@ package com.example.ecommerce.config;
 
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
-// NOT WORKING!!!
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+    private final EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         // Temporarily disable these actions so that the entities will be READ only from REST api
+        // NOT WORKING!!!
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE};
 
         config.getExposureConfiguration()
@@ -26,5 +39,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Call this method to expose the entities ids;
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200") // Accept calls from browser scripts for this origin (protocol + hostname + port)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // The url will be: .../api/products/search/findByCategoryId/{id}
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
