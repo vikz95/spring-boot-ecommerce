adbc5ab4808e14f8a8fdef3a4a4e06fa3206aaec
vikz95 <viktor.zahariev@protonmail.com>
2020-06-22 19:34:32 +0200

    Minor updates

--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200") // Accept calls from browser scripts for this origin (protocol + hostname + port)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // The url will be: .../api/products/search/findByCategoryId/{id}
+    // The url will be: .../api/products/search/findByCategoryId?id={id}
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
-    // The url will be: .../api/products/search/findByNameContaining/{name}
+    // The url will be: .../api/products/search/findByNameContaining?name={name}
     Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
